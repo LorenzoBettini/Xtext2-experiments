b8598cd694aca448bed6eef93d8bf99e5386e095
Lorenzo Bettini <bettini@dsi.unifi.it>
2011-11-22 17:25:12 +0100

    bind our own generator instead of org.eclipse.xtext.xbase.compiler.JvmModelGenerator.org.eclipse.xtext.xbase.compiler.JvmModelGenerator

--- helloxbase/org.xtext.example.helloxbase/src/org/xtext/example/helloxbase/HelloXbaseRuntimeModule.java
+++ helloxbase/org.xtext.example.helloxbase/src/org/xtext/example/helloxbase/HelloXbaseRuntimeModule.java
@@ -3,9 +3,22 @@
  */
 package org.xtext.example.helloxbase;
 
+import org.eclipse.xtext.generator.IGenerator;
+import org.xtext.example.helloxbase.generator.HelloXbaseGenerator;
+
 /**
  * Use this class to register components to be used at runtime / without the Equinox extension registry.
  */
 public class HelloXbaseRuntimeModule extends org.xtext.example.helloxbase.AbstractHelloXbaseRuntimeModule {
+
+	/**
+	 * Avoids to use the default org.eclipse.xtext.xbase.compiler.JvmModelGenerator
+	 * when using xbase.
+	 * @see org.xtext.example.helloxbase.AbstractHelloXbaseRuntimeModule#bindIGenerator()
+	 */
+	@Override
+	public Class<? extends IGenerator> bindIGenerator() {
+		return HelloXbaseGenerator.class;
+	}
 
 }
