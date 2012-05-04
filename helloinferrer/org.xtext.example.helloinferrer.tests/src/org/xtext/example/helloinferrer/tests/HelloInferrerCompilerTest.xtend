f8c63d0da6cb719b1f4c6b2b577d1e8f45e22926
Lorenzo Bettini <bettini@dsi.unifi.it>
2012-05-04 23:15:43 +0200

    initialization of output parameter

--- helloinferrer/org.xtext.example.helloinferrer.tests/src/org/xtext/example/helloinferrer/tests/HelloInferrerCompilerTest.xtend
+++ helloinferrer/org.xtext.example.helloinferrer.tests/src/org/xtext/example/helloinferrer/tests/HelloInferrerCompilerTest.xtend
@@ -5,6 +5,7 @@ import org.eclipse.xtext.junit4.InjectWith
 import org.eclipse.xtext.junit4.XtextRunner
 import org.eclipse.xtext.xbase.compiler.CompilationTestHelper
 import org.junit.Test
+import org.junit.BeforeClass
 import org.junit.runner.RunWith
 
 import static org.junit.Assert.*
@@ -14,6 +15,11 @@ import static org.junit.Assert.*
 class HelloInferrerCompilerTest {
 	
 	@Inject extension CompilationTestHelper
+	
+	@BeforeClass
+	def static void setNewLine() {
+		System::setProperty("line.separator", "\n")
+	}
 	
 	@Test
 	def void testGeneratedJavaClass() {
@@ -68,6 +74,65 @@ public class MyHello {
   public void myOp(final String s, final int i) {
     Boolean b = null; // output parameter
     b = Boolean.valueOf(true);
+  }
+}
+''')
+	}
+	
+	@Test
+	def void testInitializationOfOututParameter() {
+'''
+Hello my.test.MyHello {
+	op myOpWithClassOutputParameter(String s, int i) output Boolean b {
+		
+	}
+	
+	op myOpWithBasicOutputParameter(String s, int i) output boolean b {
+		
+	}
+}
+'''.assertCorrectJavaCodeGeneration(
+'''
+package my.test;
+
+public class MyHello {
+  public void myOpWithClassOutputParameter(final String s, final int i) {
+    Boolean b = null; // output parameter
+  }
+  
+  public void myOpWithBasicOutputParameter(final String s, final int i) {
+    boolean b = null; // output parameter
+  }
+}
+''')
+	}
+	
+	@Test
+	def void testAccessThis() {
+'''
+Hello my.test.MyHello {
+	op myOp(String s, int i) output Boolean b {
+		myOp2(true, 'foo')
+		this.myOp2(true, 'foo')
+	}
+	
+	op myOp2(boolean b, String s) output int i {
+		
+	}
+}
+'''.assertCorrectJavaCodeGeneration(
+'''
+package my.test;
+
+public class MyHello {
+  public void myOp(final String s, final int i) {
+    Boolean b = null; // output parameter
+    this.myOp2(true, "foo");
+    this.myOp2(true, "foo");
+  }
+  
+  public void myOp2(final boolean b, final String s) {
+    int i = null; // output parameter
   }
 }
 ''')
--- helloinferrer/org.xtext.example.helloinferrer.tests/xtend-gen/org/xtext/example/helloinferrer/tests/HelloInferrerCompilerTest.java
+++ helloinferrer/org.xtext.example.helloinferrer.tests/xtend-gen/org/xtext/example/helloinferrer/tests/HelloInferrerCompilerTest.java
@@ -9,6 +9,7 @@ import org.eclipse.xtext.xbase.compiler.CompilationTestHelper;
 import org.eclipse.xtext.xbase.compiler.CompilationTestHelper.Result;
 import org.eclipse.xtext.xbase.lib.Procedures.Procedure1;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.xtext.example.helloinferrer.tests.HelloInferrerInjectorProviderCustom;
@@ -19,6 +20,11 @@ import org.xtext.example.helloinferrer.tests.HelloInferrerInjectorProviderCustom
 public class HelloInferrerCompilerTest {
   @Inject
   private CompilationTestHelper _compilationTestHelper;
+  
+  @BeforeClass
+  public static void setNewLine() {
+    System.setProperty("line.separator", "\n");
+  }
   
   @Test
   public void testGeneratedJavaClass() {
@@ -109,6 +115,128 @@ public class HelloInferrerCompilerTest {
     _builder_1.newLine();
     _builder_1.append("    ");
     _builder_1.append("b = Boolean.valueOf(true);");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append("}");
+    _builder_1.newLine();
+    _builder_1.append("}");
+    _builder_1.newLine();
+    this.assertCorrectJavaCodeGeneration(_builder, _builder_1);
+  }
+  
+  @Test
+  public void testInitializationOfOututParameter() {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("Hello my.test.MyHello {");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("op myOpWithClassOutputParameter(String s, int i) output Boolean b {");
+    _builder.newLine();
+    _builder.append("\t\t");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("op myOpWithBasicOutputParameter(String s, int i) output boolean b {");
+    _builder.newLine();
+    _builder.append("\t\t");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    StringConcatenation _builder_1 = new StringConcatenation();
+    _builder_1.append("package my.test;");
+    _builder_1.newLine();
+    _builder_1.newLine();
+    _builder_1.append("public class MyHello {");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append("public void myOpWithClassOutputParameter(final String s, final int i) {");
+    _builder_1.newLine();
+    _builder_1.append("    ");
+    _builder_1.append("Boolean b = null; // output parameter");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append("}");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append("public void myOpWithBasicOutputParameter(final String s, final int i) {");
+    _builder_1.newLine();
+    _builder_1.append("    ");
+    _builder_1.append("boolean b = null; // output parameter");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append("}");
+    _builder_1.newLine();
+    _builder_1.append("}");
+    _builder_1.newLine();
+    this.assertCorrectJavaCodeGeneration(_builder, _builder_1);
+  }
+  
+  @Test
+  public void testAccessThis() {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("Hello my.test.MyHello {");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("op myOp(String s, int i) output Boolean b {");
+    _builder.newLine();
+    _builder.append("\t\t");
+    _builder.append("myOp2(true, \'foo\')");
+    _builder.newLine();
+    _builder.append("\t\t");
+    _builder.append("this.myOp2(true, \'foo\')");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("op myOp2(boolean b, String s) output int i {");
+    _builder.newLine();
+    _builder.append("\t\t");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    StringConcatenation _builder_1 = new StringConcatenation();
+    _builder_1.append("package my.test;");
+    _builder_1.newLine();
+    _builder_1.newLine();
+    _builder_1.append("public class MyHello {");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append("public void myOp(final String s, final int i) {");
+    _builder_1.newLine();
+    _builder_1.append("    ");
+    _builder_1.append("Boolean b = null; // output parameter");
+    _builder_1.newLine();
+    _builder_1.append("    ");
+    _builder_1.append("this.myOp2(true, \"foo\");");
+    _builder_1.newLine();
+    _builder_1.append("    ");
+    _builder_1.append("this.myOp2(true, \"foo\");");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append("}");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append("public void myOp2(final boolean b, final String s) {");
+    _builder_1.newLine();
+    _builder_1.append("    ");
+    _builder_1.append("int i = null; // output parameter");
     _builder_1.newLine();
     _builder_1.append("  ");
     _builder_1.append("}");
